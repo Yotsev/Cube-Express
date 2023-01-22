21d74f6e97a3459187f41c97f8ad71bd05efd6b1
Yotsev <b.yotsev@gmail.com>
2023-01-22 08:31:58 +0200

    Added layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+//3-rd party libraries
 const express = require('express');
 
+//my modules
 const setViewEngain = require('./configs/viewEngain');
 const config = require('./configs/config');
 
 const app = express();
-setViewEngain(app);
+setViewEngain(app); // setup of engine
+
+app.use(express.static('./src/public')) //Setup of static views middleware
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 })
 
 app.listen(config.PORT, () => { console.log(`Server is running on ${config.PORT}...`) });
