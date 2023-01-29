057572efab8ab5f02305077a8f60aa9e090a8f7b
Yotsev <b.yotsev@gmail.com>
2023-01-29 09:55:33 +0200

    Added new resources

--- src/routs.js
+++ src/routs.js
@@ -7,6 +7,7 @@ const router = Router();
 // We need the controllers in the router
 const cubeController = require('./controllers/cubeController'); 
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAcoutPage);
@@ -15,5 +16,8 @@ router.get('/404', homeController.getErrorPage);
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
 router.get('/details/:cubeId', cubeController.getCubeDetails);
+
+//Useing modular routs
+router.use('/accessory', accessoryController);
 
 module.exports = router;
