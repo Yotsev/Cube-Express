92361e3eaddc66dbf6c65f6d38bb8be355433957
Yotsev <b.yotsev@gmail.com>
2023-01-29 10:10:53 +0200

    Added the craete Accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
-router.get('/create', (req, res)=>{
+const Accessory = require('../models/Accessory');
+
+
+router.get('/create', (req, res) => {
     res.render('./accessory/createAccessory');
 });
 
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    await Accessory.create({ name, description, imageUrl }); // Or it can be done like in the cusecontroller
+
+    res.redirect('/');
+});
 
 module.exports = router;
