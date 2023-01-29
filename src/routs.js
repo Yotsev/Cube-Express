03171b2421558b38a599b49ff895fd89ff82abaf
Yotsev <b.yotsev@gmail.com>
2023-01-29 09:28:06 +0200

    Removed the JSONdb file and old cube model

--- src/routs.js
+++ src/routs.js
@@ -9,10 +9,10 @@ const homeController = require('./controllers/homeController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAcoutPage);
+router.get('/404', homeController.getErrorPage);
 
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
 router.get('/details/:cubeId', cubeController.getCubeDetails);
-router.get('/404', homeController.getErrorPage);
 
 module.exports = router;
